92e5f3471702357152f044c0567cfc42c45adc56
vasu Mankotia <vasu.arintech@gmail.com>
2025-02-12 17:16:12 +0530

    push all code

--- networking.tf
+++ networking.tf
@@ -5,3 +5,13 @@
      Name = "vasu-vpc"
    }
  }
+
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.main-vpc.id
+  cidr_block = "10.22.0.0/24"
+  map_public_ip_on_launch  = "true"
+  tags = {
+    Name = "vasu-subnet-1"
+  }
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output"my_vpc" {
  value = aws_vpc.main-vpc.id
 }
+
+output "my_subnet" {
+ value = aws_subnet.subnet-1.id
+}
