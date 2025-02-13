8a4da715a34f2c141f34fe74ba81b059d10b8883
vasu Mankotia <vasu.arintech@gmail.com>
2025-02-13 11:21:22 +0530

    pushing to subnet

--- networking.tf
+++ networking.tf
@@ -7,11 +7,18 @@
  }
 
 
-resource "aws_subnet" "subnet-1" {
+resource "aws_subnet""subnet-1" {
   vpc_id = aws_vpc.main-vpc.id
   cidr_block = "10.22.0.0/24"
   map_public_ip_on_launch  = "true"
   tags = {
     Name = "vasu-subnet-1"
   }
+}
+
+resource "aws_internet_gateway" "igw" {
+ vpc_id = aws_vpc.main-vpc.id
+ tags = {
+   Name = "vasu-igw"
+ }
 }
--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output"my_vpc" {
  value = aws_vpc.main-vpc.id
 }
 
-output "my_subnet" {
+output"my_subnet" {
  value = aws_subnet.subnet-1.id
+}
+
+output"internet_gateway" {
+ value = aws_internet_gateway.igw.id
 }
